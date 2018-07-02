63f6c7c951ab69b4b9cdd0ee4147262fa6df422d
buuug7 <youpp@126.com>
2018-07-02 11:34:39 +0800

    update

--- written-examination/str-remove-empty/index.js
+++ written-examination/str-remove-empty/index.js
@@ -1,5 +1,8 @@
-let str = ' abc ';
+let str = ' a bc ';
 
-let noEmpty = str.replace(/\s/gi, '');
+let noEmpty = str.replace(/(^\s*)|(\s*$)/g, '');
 
 console.log(noEmpty);
+
+// 或者使用 String.prototype.trim()
+console.log(str.trim());
